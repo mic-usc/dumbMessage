f060f43b5c1cf3138a43992f1d7089021f35e2ea
mic-usc <michelle.santos@urbansportsclub.com>
2024-12-18 12:35:26 +0100

    Initial Commit

---
+++ DIDumbMessageTest/DIDumbMessageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIDumbMessageTestApp.swift
+//  DIDumbMessageTest
+//
+//  Created by Michelle  Santos on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DIDumbMessageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DIDumbMessageTestTests/DIDumbMessageTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  DIDumbMessageTestTests.swift
+//  DIDumbMessageTestTests
+//
+//  Created by Michelle  Santos on 18/12/24.
+//
+
+import Testing
+
+struct DIDumbMessageTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
